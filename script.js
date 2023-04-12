8a41bf48c46266e558785b209491961abfd73389
Andre <123985988+derre3@users.noreply.github.com>
2023-04-12 18:21:15 -0300

    add function to check win count

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ const choices = document.querySelectorAll('.play-button');
 const start = document.querySelector('.start-button');
 let playerChoice;
 let gameActive = 0;
-let computerWins = 0;
-let playerWins = 0;
+let computerWins;
+let playerWins;
 window.onload = isGameActive();
 
 start.addEventListener('click', () => {
     gameActive = 1;
     start.style.display = "none";
     isGameActive();
+    computerWins = 0;
+    playerWins = 0;
 })
 
 choices.forEach(choice => {
@@ -33,6 +35,14 @@ function isGameActive() {
     }
 }
 
+function checkWinCount() {
+    if (playerWins === 5 || computerWins === 5) {
+        gameActive = 0;
+        isGameActive();
+        start.style.display = "block";
+    }
+}
+
 function getComputerChoice() {
    let computerChoice = Math.floor(Math.random() *3);
    if (computerChoice === 0) {
@@ -42,52 +52,31 @@ function getComputerChoice() {
    } else return 'scissors'
 }
 
-
-
-
-
-
-
-
-
-
-
-// function to play a single round of rock, paper and scissors
-//     needs 2 parameters: playerSelection and computerSelection
-//     playerSelection = getPlayerChoice()
-//     computerSelection = getComputerChoice()
-//     compare the above parameters and return a string to declare the winner
-
 function playRound() {
     computerSelection = getComputerChoice();
     playerSelection = playerChoice;
     if (computerSelection === playerSelection) {
         alert(`Draw! ${playerSelection} vs ${computerSelection}`);
     } else if (computerSelection === 'rock' && playerSelection === 'paper') {
-        playerWins++, alert(`You win the round, ${playerSelection} beats ${computerSelection}`);
+        playerWins++, alert(`You win the round, ${playerSelection} beats ${computerSelection}`),checkWinCount();
     } else if (computerSelection === 'paper' && playerSelection === 'rock') {
-        computerWins++, alert(`You lose the round, ${computerSelection} beats ${playerSelection}`);
+        computerWins++, alert(`You lose the round, ${computerSelection} beats ${playerSelection}`),checkWinCount();
     } else if (computerSelection === 'paper' && playerSelection === 'scissors') {
-        playerWins++, alert(`You win the round, ${playerSelection} beats ${computerSelection}`);
+        playerWins++, alert(`You win the round, ${playerSelection} beats ${computerSelection}`),checkWinCount();
     } else if (computerSelection === 'scissors' && playerSelection === 'paper') {
-        computerWins++, alert(`You lose the round, ${computerSelection} beats ${playerSelection}`);
+        computerWins++, alert(`You lose the round, ${computerSelection} beats ${playerSelection}`),checkWinCount();
     } else if (computerSelection === 'scissors' && playerSelection === 'rock') {
-        playerWins++, alert(`You win the round, ${playerSelection} beats ${computerSelection}`);
+        playerWins++, alert(`You win the round, ${playerSelection} beats ${computerSelection}`),checkWinCount();
     } else if (computerSelection === 'rock' && playerSelection === 'scissors') {
-        computerWins++, alert(`You lose the round, ${computerSelection} beats ${playerSelection}`);
+        computerWins++, alert(`You lose the round, ${computerSelection} beats ${playerSelection}`),checkWinCount();
     } else alert(`error`);
 }
-
-
-
-
 
 
 // function playGame() {
 //     computerWins = 0;
 //     playerWins = 0;
 //     for (let i = 0; i < 5 && computerWins < 3 && playerWins < 3; i++) {
-//         playRound();
 //         alert(`Current Score: \r Player: ${playerWins} Computer: ${computerWins}`);
 //     }
 //     if (playerWins > computerWins) {
